655ccec9c36e7b823c3f6324ca86947c4a4aac3f
Nasredine <nacer.cheniki@gmail.com>
2018-06-16 14:03:38 +0200

    resolve maven issue

--- slib-examples/src/main/java/slib/examples/sml/ld/LoadingLDGraph.java
+++ slib-examples/src/main/java/slib/examples/sml/ld/LoadingLDGraph.java
@@ -31,7 +31,7 @@
  *  The fact that you are presently reading this means that you have had
  *  knowledge of the CeCILL license and that you accept its terms.
  */
-package slib.examples.sml.owl;
+package slib.examples.sml.ld;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -47,6 +47,7 @@ import slib.graph.io.conf.GraphConf;
 import slib.graph.io.loader.GraphLoaderGeneric;
 import slib.graph.io.util.GFormat;
 import slib.graph.model.graph.G;
+import slib.graph.model.graph.elements.E;
 import slib.graph.model.impl.graph.memory.GraphMemory;
 import slib.graph.model.impl.repo.URIFactoryMemory;
 import slib.graph.model.repo.URIFactory;
@@ -62,39 +63,43 @@ import slib.utils.impl.Timer;
  *
  * @author Sébastien Harispe (sebastien.harispe@gmail.com)
  */
-public class SMComputationOWL {
+public class LoadingLDGraph {
 
     public static void main(String[] args) throws SLIB_Exception {
 
         Timer t = new Timer();
         t.start();
 
-        String ontoFile = "/home/nasredine/Documents/data/travel-domain-populated.owl";
+        String ontoFile = "/home/nasredine/Documents/data/Algeria.ntriples";
+        
+        
 
         URIFactory factory = URIFactoryMemory.getSingleton();
         URI graphURI = factory.getURI("http://graph/");
         G g = new GraphMemory(graphURI);
 
         
-        GDataConf dataConf = new GDataConf(GFormat.RDF_XML, ontoFile);
+        GDataConf dataConf = new GDataConf(GFormat.NTRIPLES, ontoFile);
+        
+        
 
         // We specify an action to root the vertices, typed as class without outgoing rdfs:subclassOf relationship 
         // Those vertices are linked to owl:Thing by an eddge x  rdfs:subClassOf owl:Thing 
-        GAction actionRerootConf = new GAction(GActionType.REROOTING);
+       // GAction actionRerootConf = new GAction(GActionType.REROOTING);
 
         // We now create the configuration we will specify to the generic loader
         GraphConf gConf = new GraphConf();
         gConf.addGDataConf(dataConf);
-        gConf.addGAction(actionRerootConf);
+      //  gConf.addGAction(actionRerootConf);
 
         GraphLoaderGeneric.load(gConf, g);
 
-        System.out.println(g.toString());
+        showVerticesAndEdges(g);
 
-        Set<URI> roots = new ValidatorDAG().getTaxonomicRoots(g);
-        System.out.println("Roots: " + roots);
+    //    Set<URI> roots = new ValidatorDAG().getTaxonomicRoots(g);
+    //    System.out.println("Roots: " + roots);
 
-
+/*
         // We compute the similarity between two concepts 
         URI countryURI = factory.getURI("https://sites.google.com/site/portdial2/downloads-area/Travel-Domain.owl#Country");
         URI cityURI = factory.getURI("https://sites.google.com/site/portdial2/downloads-area/Travel-Domain.owl#City");
@@ -116,34 +121,25 @@ public class SMComputationOWL {
          * Let's perform numerous random computations (we only print some results).
          * We retrieve the set of vertices as a list
          */
-        int totalComparison = 10;
 
-        List<URI> listVertices = new ArrayList<URI>(GraphAccessor.getClasses(g));
-       
-        
-        int nbConcepts = listVertices.size();
-        int id1, id2;
-        URI c1, c2;
-        String idC1, idC2;
-        Random r = new Random();
-
-        for (int i = 0; i < totalComparison; i++) {
-            id1 = r.nextInt(nbConcepts);
-            id2 = r.nextInt(nbConcepts);
-
-            c1 = listVertices.get(id1);
-            c2 = listVertices.get(id2);
-
-            sim = engine.compare(smConf, c1, c2);
-
-//            if ((i + 1) % 1000 == 0) {
-                idC1 = c1.getLocalName();
-                idC2 = c2.getLocalName();
-
-                System.out.println("Sim " + (i + 1) + "/" + totalComparison + "\t" + idC1 + "/" + idC2 + ": " + sim);
-//            }
-        }
         t.stop();
         t.elapsedTime();
+    }
+    
+  private static void showVerticesAndEdges(G graph) {
+        
+        
+        Set<URI> vertices = graph.getV();
+        Set<E> edges = graph.getE();
+        
+        System.out.println("-Vertices");
+        for(URI v : vertices){
+            System.out.println("\t"+v);
+        }
+        
+        System.out.println("-Edge");
+        for(E edge : edges){
+            System.out.println("\t"+edge);
+        }
     }
 }
